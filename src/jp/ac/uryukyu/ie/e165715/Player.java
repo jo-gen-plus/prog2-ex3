26641524cb5bfdc55aa63fda037b4ee260df683c
e165715 <e165715@ie.u-ryukyu.ac.jp>
2016-12-06 11:53:47 +0900

    test

---
+++ src/jp/ac/uryukyu/ie/e165715/Player.java
@@ -0,0 +1,20 @@
+package jp.ac.uryukyu.ie.e165715;
+
+/**
+ * Created by e165715 on 2016/12/06.
+ */
+public class Player {
+    //getLabyrinthでリストを取り込む，
+    //w,a,s,dそれぞれの場合，(0,1)(-1,0)(0,-1)(1,0)とする
+    //move(x座標,y座標)とする
+    //moveの中身としては，x座標の場合，stringクラスの　　　メソッドを，
+    //                  y座標の場合リストの要素の何番目かを指定。
+    public List<String> map = getLabyrinth();
+    public void move (int x,int y){
+
+    }
+
+
+
+
+}
