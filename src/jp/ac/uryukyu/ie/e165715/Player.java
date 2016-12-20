10e35da634da0f58d741156a767296045d2abc01
e165715 <e165715@ie.u-ryukyu.ac.jp>
2016-12-20 09:56:42 +0900

    test

--- src/jp/ac/uryukyu/ie/e165715/Player.java
+++ src/jp/ac/uryukyu/ie/e165715/Player.java
@@ -9,20 +9,36 @@ import java.util.ArrayList; // Listの実装
 import java.util.List; // いわゆる「リスト構造」のインターフェース（実装のないクラス。教科書19章）
 import java.util.Scanner; // 標準入力読み込みの際に利用したクラス
 
-public class Player {
+public class Player extends Labyrinth{
     //getLabyrinthでリストを取り込む，
     //w,a,s,dそれぞれの場合，(0,1)(-1,0)(0,-1)(1,0)とする
     //move(x座標,y座標)とする
     //moveの中身としては，x座標の場合，stringクラスの　　　メソッドを，
     //                  y座標の場合リストの要素の何番目かを指定。
     public List<String> map = getLabyrinth();
-    public void Player(String x){
+
+    //public List<StringBuilder> map1 = (StringBuilder) map;
+
+
+
+
+    public Player(String x){
+        if (x == "w"){
+
+        }else if(x == "a"){
+
+        }else if(x == "s"){
+
+        }else if(x == "d"){
+
+        }else{
+            System.out.println("w,a,s,dのうちどれか一文字だけ入力してください");
+        }
 
     }
 
-    public void move (int x,int y){
 
-    }
+
 
 
 
